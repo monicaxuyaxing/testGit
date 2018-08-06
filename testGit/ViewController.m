32fa0cb6204b399491ad3fc265b71eefe557ce59
monicaxuyaxing <594187213@qq.com>
2018-08-06 21:32:05 +0800

    master第一次提交

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"111");
+    NSLog(@"222");
     NSString *m1 = @"";
     objc_setAssociatedObject(self, &m1, @"1", OBJC_ASSOCIATION_COPY_NONATOMIC);
     NSLog(@"value1: %@",objc_getAssociatedObject(self, &m1));
