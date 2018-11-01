9399f6b5a2babed0b5d0f7c1136046a614e86497
“xuyaxing” <xuyaxing@wsdashi.com>
2018-11-01 09:51:37 +0800

    添加123

--- testGit/Person.m
+++ testGit/Person.m
@@ -20,6 +20,7 @@
      监听上下文设置为nil，相信很多人在使用的时候都会这么写
      */
     [self.account addObserver:self forKeyPath:@"balance" options:NSKeyValueObservingOptionNew context:nil];
+    NSLog(@"123");
 }
 
 //监听器回调方法
